86a3830c65e76cf13125fa3e73754fae7cc59894
MeCheckCode <michal.soboszek@everyflow.pl>
2024-10-17 14:40:41 +0200

    Death report

---
+++ composables/checkReport.ts
@@ -0,0 +1,18 @@
+export const checkReport = async (userId) => {
+    const data = ref(null);
+    const error = ref(null);
+    const loading = ref(true);
+    try {
+        const response = await $fetch(`/api/checkReport/${userId}`);
+        if (response.success) {
+            data.value = response.data;
+        } else {
+          throw new Error(response.error);
+        }
+      } catch (err) {
+        error.value = err.message;
+      } finally {
+        loading.value = false;
+      }
+      return { data, error, loading };
+  }

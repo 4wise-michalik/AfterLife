810546c1ff4da49aa67c717572bf52b81836739c
MeCheckCode <michal.soboszek@everyflow.pl>
2024-10-24 12:46:58 +0200

    Prettier

--- components/SectionGroup.vue
+++ components/SectionGroup.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 defineProps({
   posts: Array,
-  title: String
-})
+  title: String,
+});
 </script>
 
-
 <template>
-    <section class="bg-purple-900 text-white p-7 rounded-lg shadow-lg my-5">
-        <h2 class="text-xl mb-4">{{ title }}</h2>
-      <div class="flex flex-wrap -mx-4">
-        <div v-for="(post, index) in posts" :key="index" class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-          <Post :content="post.content" :time="post.time" />
-        </div>
+  <section class="bg-purple-900 text-white p-7 rounded-lg shadow-lg my-5">
+    <h2 class="text-xl mb-4">{{ title }}</h2>
+    <div class="flex flex-wrap -mx-4">
+      <div
+        v-for="(post, index) in posts"
+        :key="index"
+        class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8"
+      >
+        <Post :content="post.content" :time="post.time" />
       </div>
-    </section>
-  </template>
-  
-  
+    </div>
+  </section>
+</template>
--- pages/home/posts/index.vue
+++ pages/home/posts/index.vue
@@ -1,28 +1,53 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'withsidebar'
-})
+  layout: "withsidebar",
+});
 
 // Group posts into sections
 const groupedPosts = {
   Facebook: [
-    {content: "Lorem ipsum dolor sit amet,  tempor incididunt ut labuat.", time: "1 day" },
-    {content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis  do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", time: "1 day" },
-    {content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", time: "1 day" },
-    {content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", time: "3 days" }
+    {
+      content: "Lorem ipsum dolor sit amet,  tempor incididunt ut labuat.",
+      time: "1 day",
+    },
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis  do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      time: "1 day",
+    },
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      time: "1 day",
+    },
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      time: "3 days",
+    },
   ],
   Instagram: [
-    {content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ", time: "Immediately" },
-    {content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ", time: "1 week" }
-  ]
-}
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      time: "Immediately",
+    },
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      time: "1 week",
+    },
+  ],
+};
 </script>
 
-
 <template>
-    <div class="container mx-auto px-4 py-8">
-      <SectionGroup v-for="(posts, title) in groupedPosts" :key="title" :posts="posts" :title="title" />
-
+  <div class="container mx-auto px-4 py-8">
+    <SectionGroup
+      v-for="(posts, title) in groupedPosts"
+      :key="title"
+      :posts="posts"
+      :title="title"
+    />
   </div>
 </template>
-

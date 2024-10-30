968b3e618402025eee8d34459d54232861497d0f
MeCheckCode <michal.soboszek@everyflow.pl>
2024-10-30 11:38:40 +0100

    Pinia and auth-utils setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
-  css: [
-    '@/assets/css/global.css',
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "nuxt-auth-utils",
   ],
-})
+  css: ["@/assets/css/global.css"],
+});
